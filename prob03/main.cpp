3e4425e872ed834e30a7641a7c5013baad9e244a
avahess <avahess@cs104u-3f.acad.fullerton.edu>
2019-09-21 15:13:36 -0700

    created a random rectangle

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,27 @@
 // Random Rectangle
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+
+
+  const int LENGTH = 40;
+
+  std::string max;
+
+  unsigned seed = time(0);
+  srand(seed);
+  int length = rand() % LENGTH + 1;
+
+  max.assign(length, '#');
+  std::cout << max << std::endl;
+  std::cout << max << std::endl;
+  std::cout << max << std::endl;
+
+
+  return 0;
+}
